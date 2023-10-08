f94a49f5d61cc40575af1df989c2c3518b78c55b
breinerm25 <65035284+breinerm25@users.noreply.github.com>
2023-10-08 15:37:35 -0500

    a

--- C_assistant.py
+++ C_assistant.py
@@ -8,6 +8,7 @@ from gtts import gTTS
 import random
 ##import openai
 
+col1, col2 = st.columns(2)
 translator = Translator()
 frases_motivacionales = [
     "La determinación es la clave del éxito.",
@@ -26,29 +27,35 @@ frases_motivacionales = [
 st.header('HOW U DOING (bot)')
 st.write("Este bot analiza por medio de IA la carga emocional de tu respuesta, con base en ello, genera respuestas afines para crear un hábito de pensamientos positivos")
 
-
-
-with st.chat_message("user"):
+with col1:
+    with st.chat_message("user"):
     st.write("¡Hola!, ¿Cómo te sientes hoy? 👋")
 
-with st.expander('Escribe aquí'):
-    text = st.text_input('')
-    if text:
-        translation = translator.translate(text, src="es", dest="en")
-        trans_text = translation.text
-        blob = TextBlob(trans_text)
-        x= round(round(blob.sentiment.polarity,2) / (blob.sentiment.subjectivity+0.001),2)
-        st.write("Tú respuesta corresponde a: " , x , "puntos")
+    
+    with st.expander('Escribe aquí'):
+        text = st.text_input('')
+        if text:
+            translation = translator.translate(text, src="es", dest="en")
+            trans_text = translation.text
+            blob = TextBlob(trans_text)
+            x= round(round(blob.sentiment.polarity,2) / (blob.sentiment.subjectivity+0.001),2)
+            st.write("Tú respuesta corresponde a: " , x , "puntos")
+            
+            #sObjetivo.apppend(x)
+            iFrase = random.randint(0, len(frases_motivacionales) - 1)
+            st.write("Frases diaria")
+
+            if x >= 0.3:
+                st.write( 'Es un sentimiento Positivo 😊')
+            elif x <= -0.2:
+                st.write(frases_motivacionales[iFrase])
+            else:
+                st.write('Si necesitas ayuda, no dudes en pedirla. Estoy para escucharte si me dices un poco más')
+
         
-        #sObjetivo.apppend(x)
-        iFrase = random.randint(0, len(frases_motivacionales) - 1)
-        st.write("Frases diaria")
+with col2:
+    st.title("ZONA NUEVA")
 
-        if x >= 0.3:
-            st.write( 'Es un sentimiento Positivo 😊')
-        elif x <= -0.2:
-            st.write(frases_motivacionales[iFrase])
-        else:
-            st.write('Si necesitas ayuda, no dudes en pedirla. Estoy para escucharte si me dices un poco más')
 
-       
+
+
