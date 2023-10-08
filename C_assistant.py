999b19d4c5625edb245c0941fa47fa047c8a4408
breinerm25 <65035284+breinerm25@users.noreply.github.com>
2023-10-08 13:25:56 -0500

    Update C_assistant.py

--- C_assistant.py
+++ C_assistant.py
@@ -2,12 +2,12 @@ from textblob import TextBlob
 import pandas as pd
 import streamlit as st
 from googletrans import Translator
-import openai
+##import openai
 
 translator = Translator()
 ##Seteando IA
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-if "openai_model" not in st.session_state:
+##openai.api_key = st.secrets["OPENAI_API_KEY"]
+##if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 st.header('HOW U DOING (bot)')
@@ -24,9 +24,15 @@ with st.expander('Escribe aquí'):
         st.write('Polarity: ', round(blob.sentiment.polarity,2))
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
         x=round(blob.sentiment.polarity,2)
+        polaridades = []
+        polaridades.apppend(x)
+        
         if x >= 0.5:
             st.write( 'Es un sentimiento Positivo 😊')
         elif x <= -0.5:
             st.write( 'Es un sentimiento Negativo 😔')
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+
+hist_values = np.histogram(polaridades, bins=24, range=(0,24))[0]
+        
